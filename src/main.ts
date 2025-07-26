d736482243dd5495f6ecae9d7acf6c88f117ed32
aayushkakkar26 <aayush90793@gmail.com>
2025-07-26 09:27:14 +0530

    Changes

--- src/main.ts
+++ src/main.ts
@@ -5,9 +5,9 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   
   app.enableCors({
-    origin: 'http://localhost:3001',
-    credentials: true,
-  });
+  origin: ['http://localhost:3001', 'http://51.21.152.45:3001'],
+  credentials: true,
+});
   app.use(json());
   app.use(urlencoded({ extended: true }));
   await app.listen(process.env.PORT ?? 3000);
